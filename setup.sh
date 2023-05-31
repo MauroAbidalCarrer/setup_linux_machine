ed35aea29124f817d830fede5eec4aff6e863cda
Mauro Abidal Carrer <60320694+MauroAbidalCarrer@users.noreply.github.com>
2023-05-31 13:13:20 -0500

    Added VScode intalling

--- setup.sh
+++ setup.sh
@@ -38,3 +38,16 @@ echo "alias install='sudo apt-get update && sudo apt-get install'" >> ~/.zshrc
 echo "alias status='git status'" >> ~/.zshrc
 echo "alias log='git log --all --graph --decorate --oneline --simplify-by-decoration'" >> ~/.zshrc
 echo "alias commit='git commit -am'" >> ~/.zshrc
+
+
+#install VScode
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+
+
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code # or code-insiders
