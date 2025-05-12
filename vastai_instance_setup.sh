718a5cd3b33d2f46e3b1de25164e3e1d2ccefe0d
Mauro Abidal Carrer <mauroabida@yahoo.fr>
2025-05-12 17:32:34 +0200

    Feat: Added new vastai_instance_setup.sh script.

---
+++ vastai_instance_setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+sudo apt-get install -y git curl wget zsh 
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "exec zsh" >> ~/.bashrc
+
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm ~/miniconda3/miniconda.sh
+source ~/miniconda3/bin/activate
+conda init --all
+
+#aliases
+echo "alias update='sudo apt-get update'" >> ~/.zshrc
+echo "alias install='sudo apt-get update && sudo apt-get install'" >> ~/.zshrc
+echo "alias status='git status -s'" >> ~/.zshrc
+echo "alias am='git commit -am'" >> ~/.zshrc
+echo "alias m='git commit -m'" >> ~/.zshrc
+echo "alias recreate_conda_env='function _recreate_conda_env(){ conda deactivate && conda env remove -yn "$1"; conda env create -n "$1" -f "$2" && conda activate "$1"; }; _recreate_conda_env'" >> ~/.zshrc
+
