beb0465a54d638bdb5c78f723810698989e0488f
Mauro Abidal Carrer <60320694+MauroAbidalCarrer@users.noreply.github.com>
2025-05-22 14:34:20 +0200

    Feat: Added commands to vastai_instance_setup to add github to known hosts.

--- vastai_instance_setup.sh
+++ vastai_instance_setup.sh
@@ -21,6 +21,9 @@ touch ~/.no_auto_tmux
 mkdir -p ~/repos
 git config --global user.email "mauroabidal@yahoo.fr"
 git config --global user.name  "Mauro Abidal Carrer"
+mkdir -p ~/.ssh
+ssh-keygen -F github.com > /dev/null || ssh-keyscan github.com >> ~/.ssh/known_hosts
 
 # Setup aliases
 wget https://raw.githubusercontent.com/MauroAbidalCarrer/setup_linux_machine/refs/heads/master/aliases.zsh -O /root/.oh-my-zsh/custom/aliases.zsh
+
