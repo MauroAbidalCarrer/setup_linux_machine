d484c6d14a5149c083162c9b2ebcfdd4b7d93655
Mauro Abidal Carrer <60320694+MauroAbidalCarrer@users.noreply.github.com>
2025-06-14 11:15:45 +0200

    Update aliases.zsh

--- aliases.zsh
+++ aliases.zsh
@@ -83,5 +83,5 @@ conda deactivate && conda env remove -yn "$1"; conda env create -n "$1" -f "$2" 
 alias install='sudo apt-get update && sudo apt-get install'
 alias l1='ls -1'
 alias unpack='sudo dpkg -i'
-alias update_aliases="wget -O ~/.oh-my-zsh/custom/aliases.zsh https://raw.githubusercontent.com/MauroAbidalCarrer/setup_linux_machine/refs/heads/master/aliases.zsh"
+alias update_aliases="wget -q -O ~/.oh-my-zsh/custom/aliases.zsh https://raw.githubusercontent.com/MauroAbidalCarrer/setup_linux_machine/refs/heads/master/aliases.zsh"
 alias search="conda search"
