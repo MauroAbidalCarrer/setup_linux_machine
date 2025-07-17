9f9cd9ed333eeda304873745dbb5a1da10faf67b
Mauro Abidal Carrer <60320694+MauroAbidalCarrer@users.noreply.github.com>
2025-07-17 17:49:12 +0200

    Update vastai_instance_setup.sh

--- vastai_instance_setup.sh
+++ vastai_instance_setup.sh
@@ -4,12 +4,17 @@ sudo apt-get install -y git curl wget zsh ncdu tree
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 echo "exec zsh" >> ~/.bashrc
 
+# conda
 mkdir -p ~/miniconda3
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
 bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
 rm ~/miniconda3/miniconda.sh
 source ~/miniconda3/bin/activate
 conda init --all
+conda tos accept --override-channels --channel nvidia
+conda tos accept --override-channels --channel pytorch
+conda tos accept --override-channels --channel conda-forge
+conda tos accept --override-channels --channel defaults
 
 #zshrc
 echo "cd ~/" >> ~/.zshrc
