4ef7c749befc309ad2706df523ef11e483e518ce
Mauro Abidal Carrer <60320694+MauroAbidalCarrer@users.noreply.github.com>
2025-11-10 13:08:02 +0100

    switched to uv

--- vastai_instance_setup.sh
+++ vastai_instance_setup.sh
@@ -4,18 +4,8 @@ sudo apt-get install -y git curl wget zsh ncdu tree
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 echo "exec zsh" >> ~/.bashrc
 
-# conda
-mkdir -p ~/miniconda3
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
-bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
-rm ~/miniconda3/miniconda.sh
-source ~/miniconda3/bin/activate
-conda init --all
-conda tos accept --override-channels --channel nvidia
-conda tos accept --override-channels --channel pytorch
-conda tos accept --override-channels --channel conda-forge
-conda tos accept --override-channels --channel defaults
-
+# uv
+curl -LsSf https://astral.sh/uv/install.sh | sh
 #zshrc
 echo "cd ~/" >> ~/.zshrc
 
