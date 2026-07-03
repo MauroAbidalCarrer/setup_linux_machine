9b96298fb65a383a0a25753662aec25ae052d540
Mauro Abidal Carrer <60320694+MauroAbidalCarrer@users.noreply.github.com>
2026-07-03 12:16:18 +0200

    Update vastai_instance_setup.sh

--- vastai_instance_setup.sh
+++ vastai_instance_setup.sh
@@ -32,3 +32,8 @@ curl -fsSL https://opencode.ai/install | bash
 
 # Remove banner print on ssh connection
 touch .hushlogin
+
+# aws 
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
