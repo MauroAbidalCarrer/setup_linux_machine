f117602e8d820debfa8b9c48418e9a9304736655
Mauro Abidal Carrer <60320694+MauroAbidalCarrer@users.noreply.github.com>
2026-08-14 11:00:14 +0200

    Update vastai_instance_setup.sh

--- vastai_instance_setup.sh
+++ vastai_instance_setup.sh
@@ -37,3 +37,6 @@ touch ~/.hushlogin
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
+
+
+curl "https://raw.githubusercontent.com/MauroAbidalCarrer/setup_linux_machine/refs/heads/master/vimrc" -o "~/.vimrc"
